23e103b5f9af88ecc3f2b4ff3bb5c66825c36967
angelo-rbs <angeloraphael1@hotmail.com>
2024-08-22 17:47:00 -0300

    initial commit

---
+++ src/main/java/com/lamarca/ivan/IvanApplication.java
@@ -0,0 +1,12 @@
+package com.lamarca.ivan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lamarca/ivan/IvanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamarca.ivan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
